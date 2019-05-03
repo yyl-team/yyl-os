93cb8395a4baef73273dd77c16e34894f47707df
jackness <jackness1208@qq.com>
2019-05-03 21:28:53 +0800

    * [ADD] 新增 `extOs.installPackage(pkgPath)` 方法

--- lib/os.d.ts
+++ lib/os.d.ts
@@ -14,5 +14,6 @@ interface Ios {
   runCMD(cmd: string, path?:string, showOutput?: boolean, newWindow?: boolean): Promise<any>;
   runSpawn(cmd: string, path?:string, showOutput?: boolean): Promise<any>;
   installNodeModules(plugins: string[], basePath: string): Promise<any>;
+  installPackage(pkgPath: string): Promise<any>;
 }
 export = os;
